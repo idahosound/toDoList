f60181b29447c842e257176b93d3071e7a568de1
John McMahon <idahosound@gmail.com>
2020-08-29 16:35:22 -0600

    add start of mongo db functionality

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const date = require(__dirname + '/views/date.js');
+
+mongoose.connect('mongodb://localhost:27017/todoListDB', {useNewUrlParser: true, useUnifiedTopology: true });
+const itemSchema = {
+  name: String
+};
+const Item = mongoose.model ("item", itemSchema);
+// Add Default MongoDB items here
+// const wake = new Item ({
+//   name: "Wake Up"
+// });
+// const rise = new Item ({
+//   name: "Get Out of Bed"
+// });
+// const comb = new Item ({
+//   name: "Drag a Comb Across Yer Head"
+// });
+// Item.insertMany([wake, rise, comb], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Items added');
+//   };
+// });
+
+
 let items = [];
 let workItems = [];
 let dayInfo = date.date();
 let day = dayInfo.day;
 let publishYear = dayInfo.year;
+
+let itemList = Item.find(function(err, item){
+  if (err){
+    console.log(err);
+  } else {
+    item.forEach(function(item){
+      items.push(item.name)
+    });
+  };
+});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
